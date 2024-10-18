3f6f484a5b2e63722944507cfd881f9aec85a246
John Arundel <john@bitfieldconsulting.com>
2024-10-18 11:11:54 +0100

    tweak docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,48 @@
+//! `up2code` is a tool for checking code listings in Markdown files, to make
+//! sure they're up to date with, and in sync with, canonical versions stored in
+//! a GitHub repo.
+//!
+//! `up2code` reads all the Markdown files you specify, looking for what it
+//! considers a “listing”: a fenced code block immediately followed by a web
+//! link. For example:
+//!
+//! ```text
+//!     ```rust
+//!     fn main() {
+//!         println!("Hello, world!")
+//!     }
+//!     ```
+//!     [Listing `hello/1`](https://github.com/bitfield/example/blob/src/main.rs))
+//! ```
+//!
+//! It will try to fetch the raw code page from the specified URL (appending
+//! "?raw=true"), reporting any errors. If the fetch succeeds, it will check
+//! that the Markdown listing is an exact substring of the GitHub listing,
+//! reporting any mismatch as a unified diff:
+//!
+//! ```text
+//! tests/data/test.md: Listing `counter_2`
+//! @@ -6,8 +13,8 @@
+//!
+//!      #[test]
+//!      fn count_lines_fn_counts_lines_in_input() {
+//! -        let input = io::Cursor::new("line 1\nline2\n");
+//! +        let input = io::Cursor::new("line 1\nline 2\n");
+//!          let lines = count_lines(input);
+//! -        assert_eq!(2, lines);
+//! +        assert_eq!(lines, 2);
+//!      }
+//!  }
+//! ```
+
 use regex::Regex;
 use similar::TextDiff;
 
 use std::{fs, io, path::Path, sync::LazyLock};
 
 static LISTINGS: LazyLock<Regex> = LazyLock::new(|| {
-    Regex::new(
-        r"(?m)^```.*?\n(?<code>[^`]+?\n)?```\n\(\[(?<title>[\s\S]+?)\]\((?<link>.*?)\)",
-    )
-    .unwrap()
+    Regex::new(r"(?m)^```.*?\n(?<code>[^`]+?\n)?```\n\(\[(?<title>[\s\S]+?)\]\((?<link>.*?)\)")
+        .unwrap()
 });
 
 static HTTP: LazyLock<reqwest::blocking::Client> =
@@ -37,6 +72,7 @@ pub fn listings(path: impl AsRef<Path>) -> io::Result<Vec<Listing>> {
     Ok(listings)
 }
 
+/// A listing as parsed out of the Markdown source.
 pub struct Listing {
     pub title: String,
     pub code: String,
@@ -44,7 +80,7 @@ pub struct Listing {
 }
 
 impl Listing {
-    /// Fetches the canonical listing from its URL and stores the text.
+    /// Fetches the canonical version of the listing from its GitHub URL.
     ///
     /// # Errors
     ///
@@ -55,11 +91,13 @@ impl Listing {
         Ok(CheckedListing {
             title: self.title,
             code: self.code,
-            text: resp.text()?
+            text: resp.text()?,
         })
     }
 }
 
+/// A checked listing containing both the Markdown version and the canonical
+/// version from GitHub.
 pub struct CheckedListing {
     pub title: String,
     pub code: String,
@@ -68,6 +106,7 @@ pub struct CheckedListing {
 
 impl CheckedListing {
     #[must_use]
+    /// Diffs the Markdown listing against its canonical GitHub version.
     pub fn diff(&self) -> Option<String> {
         if self.text.contains(&self.code) {
             None
